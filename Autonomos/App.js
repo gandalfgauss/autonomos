2790eb10c6c35fce112c2708fa0e9882affc39e3
gandalfgauss <hallidaysantos@gmail.com>
2023-01-30 09:29:17 -0300

    Criado o projeto em React Native

---
+++ Autonomos/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+
+export default function App(){
+  return(
+
+    <View>
+      <Text> Autônomos</Text>
+    </View>
+  )
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# Projeto Autônomos
