635a4f63b5a0ffa872cbc0c390b4aedc61e03003
gandalfgauss <hallidaysantos@gmail.com>
2023-03-13 14:53:19 -0300

    Alterando tela de notificacao de autonomos

--- Autonomos/Api.js
+++ Autonomos/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-export const ApiUrl = "http://192.168.2.7:3000"
+export const ApiUrl = "http://192.168.2.6:3000"
 
 export const Api = axios.create({
-    baseURL: "http://192.168.2.7:3000"
+    baseURL: "http://192.168.2.6:3000"
 })
